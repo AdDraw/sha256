2ec87f59d1b0d092d270eac4c8e8beea6840069c
Adam <adam@plumerai.com>
2021-01-25 05:46:38 +0100

    Make SHA25 testbench more PG

--- sim/tb_sha256.vhd
+++ sim/tb_sha256.vhd
@@ -72,12 +72,6 @@ ARCHITECTURE behavior OF test_sha256 IS
 	constant chunk_aga		: slv_arr16_8 := (x"61676120", x"80000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000",
 															x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000020");
 
-	constant chunk_naga		: slv_arr16_8 := (x"6E616761", x"80000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000",
-															x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000020");
-
-	constant chunk_aga_naga : slv_arr16_8 := (x"61676120", x"6E616761", x"80000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000",
-															x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000000", x"00000040");
-
 	constant chunker_p1 : slv_arr16_8 := (x"31323334", x"35363738",
 													  x"31323334", x"35363738",
 													  x"31323334", x"35363738",
@@ -190,7 +184,7 @@ BEGIN
 				report "Hash of 'abc' is not ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad!";
 		report "Val_Of_HASH: " & to_hstring(hash); 
 	  		
-		-- 1. send aga_ chunk
+		-- 1. send aga chunk
 		chunk_id  <= (others => '0');
 		Mem <= chunk_aga;
 	  
@@ -199,30 +193,9 @@ BEGIN
 		assert hash = x"e13fb754f56b8a729d9a54e878e692eda25658ff5ce756004dd3fd22ef3e863a"
 			report "Hash of 'aga ' is not e13fb754f56b8a729d9a54e878e692eda25658ff5ce756004dd3fd22ef3e863a!";
 		report "Val_Of_HASH: " & to_hstring(hash); 
-
-		-- 2. send naga chunk, inc the chunk_id
-		chunk_id  	<= (others => '0');
-		Mem <= chunk_naga;
-	  
-		wait until vld_hash = '1' and vld_hash'stable;
-	  
-		assert hash = x"55235d10c95c19c7bb7dfd3f9f2cbac184b8662ae36b009a26005070b80b717f"
-			report "Hash of 'naga' is not 55235d10c95c19c7bb7dfd3f9f2cbac184b8662ae36b009a26005070b80b717f!";
-		report "Val_Of_HASH: " & to_hstring(hash); 
-
-		--3 send aga_naga chunk
-		
-		chunk_id  	<= (others => '0');
-		Mem <= chunk_aga_naga;
-	  
-		wait until vld_hash = '1' and vld_hash'stable;
-	  
-		assert hash = x"e1f67b2a5c62af7d40bd7966211f339b24f9f80eaf7e07fb064920ab78e49c7e"
-			report "Hash of 'aga naga ' is not e1f67b2a5c62af7d40bd7966211f339b24f9f80eaf7e07fb064920ab78e49c7e!";
-		report "Val_Of_HASH: " & to_hstring(hash); 
 		
 		
-		--4 BIG CHUNKO
+		--2 full_chunk
 		chunk_id  	<= (others => '0');
 		Mem <= chunker_p1;
 	  
@@ -233,12 +206,13 @@ BEGIN
 		report "Val_Of_HASH: " & to_hstring(hash); 
 		
 		
-		--5. 2 CHUNKO BUNGO
+		--3. 2 CHUNKs 1st chunk
 		chunk_id  	<= (others => '0');
 		Mem <= p1;
 	  
 		wait until vld_hash = '1' and vld_hash'stable;
 		
+		--3. 2 CHUNKs 2nd chunk
 		chunk_id  	<= (others => '0');
 		chunk_id(0)	<= '1';
 		Mem <= p2;
@@ -249,7 +223,7 @@ BEGIN
 			report "Hash of 'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq' is not 248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1!";
 		report "Val_Of_HASH: " & to_hstring(hash); 
 		
-		--6 BIG CHUNKO 2nd attempt
+		--4 Full chunk
 		chunk_id  	<= (others => '0');
 		Mem <= p1_small;
 	  
